a106fb189cfc38f31ce017fc0248da0ad466ced6
alexUfa2 <barracuda-3@yandex.ru>
2019-07-06 21:08:10 +0500

    КОнтроллер, правка после мерлжа

--- Backend/BackendProject/Controllers/ExchangeDataController.cs
+++ Backend/BackendProject/Controllers/ExchangeDataController.cs
@@ -9,19 +9,19 @@ using TemplateApp.Services;
 
 namespace TemplateApp.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class ExchangeDataController : ControllerBase
-	{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExchangeDataController : ControllerBase
+    {
 
-		private IConfiguration _configuration;
-		private IDataGovRuService _dataGovRuService;
+        private IConfiguration _configuration;
+        private IDataGovRuService _dataGovRuService;
 
         private IFileService _fileService;
         private ITextFileParser _parser;
 
 
-<<<<<<< HEAD
+
         public ExchangeDataController(IConfiguration configuration, IDataGovRuService dataGovRuService,
             IFileService fileService, ITextFileParser parser)
         {
@@ -31,30 +31,22 @@ namespace TemplateApp.Controllers
             _parser = parser;
 
         }
-=======
-		public ExchangeDataController(IConfiguration configuration, IDataGovRuService dataGovRuService)
-		{
-			_configuration = configuration;
-			_dataGovRuService = dataGovRuService;
-		}
->>>>>>> c25c75f128553a2daea9cc94647dedc36e4ff9d1
 
+        [HttpGet("importdatagovrudata")]
+        [IgnoreAntiforgeryToken]
+        public async Task<IActionResult> ImportDataGovRuData(int number)
+        {
 
-		[HttpGet("importdatagovrudata")]
-		[IgnoreAntiforgeryToken]
-		public async Task<IActionResult> ImportDataGovRuData(int number)
-		{
+            var result = await _dataGovRuService.GetEntries();
 
-			var result = await _dataGovRuService.GetEntries();
+            string id = result[number].identifier;
 
-			string id = result[number].identifier;
+            var data = await _dataGovRuService.GetLastVersionForEntry(id);
 
-			var data = await _dataGovRuService.GetLastVersionForEntry(id);
+            var res2 = await _dataGovRuService.GetRows(id, data);
 
-			var res2 = await _dataGovRuService.GetRows(id, data);
-
-			return Ok(res2);
-		}
+            return Ok(res2);
+        }
 
         [IgnoreAntiforgeryToken]
         [HttpGet("test")]
@@ -63,6 +55,6 @@ namespace TemplateApp.Controllers
             var result = _parser.ParseFile(@"c:\Hackaton\Дата.Гов\data-20190703T0648-structure-20190703T0648.csv");
 
             return Ok("");
-	}
-}
+        }
+    }
 }
